cafb4799f5d1159102637700cdb81d0f98ce6db3
podhmo <ababjam61+github@gmail.com>
2019-01-02 17:35:27 +0900

    tests: assersion with mock

--- user/endpoint_test.go
+++ user/endpoint_test.go
@@ -12,12 +12,13 @@ import (
 
 func TestUserServiceEndpoint(t *testing.T) {
 	id := "1"
-	ep := &user.UserEndpoint{
-		Service: &vmock.UserService{
-			GetFunc: func(id string) (*user.User, error) {
-				return &user.User{ID: id}, nil
-			},
+	service := &vmock.UserService{
+		GetFunc: func(id string) (*user.User, error) {
+			return &user.User{ID: id}, nil
 		},
+	}
+	ep := &user.UserEndpoint{
+		Service: service,
 	}
 
 	req := httptest.NewRequest("GET", fmt.Sprintf("http://example.com/users/%s", id), nil)
@@ -27,5 +28,14 @@ func TestUserServiceEndpoint(t *testing.T) {
 	res := w.Result()
 	if res.StatusCode != http.StatusOK {
 		t.Errorf("expected status is %d, but %d", 200, res.StatusCode)
+	}
+
+	// spy (mock)
+	if !service.GetCalled() {
+		t.Error("Get() is must be called, but not called")
+	}
+
+	if service.GetCalls()[0].Id != id {
+		t.Errorf("passed id is must be %s, but %s", id, service.GetCalls()[0].Id)
 	}
 }
